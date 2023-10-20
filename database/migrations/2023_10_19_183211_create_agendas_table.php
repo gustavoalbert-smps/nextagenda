fcdb7dada88ce5345009c42ad5fc47a984db34a7
albertsousa <gustavoads.millionary@gmail.com>
2023-10-20 17:19:23 -0300

    add axios, pinia and vue-router to project;

--- database/migrations/2023_10_19_183211_create_agendas_table.php
+++ database/migrations/2023_10_19_183211_create_agendas_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
-            $table->unsignedBigInteger('tag_id');
-            $table->foreign('tag_id')->references('id')->on('tags');
+            // $table->unsignedBigInteger('tag_id');
+            // $table->foreign('tag_id')->references('id')->on('tags');
             $table->string('name');
             $table->boolean('status');
             $table->timestamps();
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,16 @@
 import './assets/main.css'
+import './axios'
 
 import { createApp } from 'vue'
-import App from './App.vue'
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import router from './router'
+
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(pinia)
+app.use(router)
+
+app.mount('#app')
